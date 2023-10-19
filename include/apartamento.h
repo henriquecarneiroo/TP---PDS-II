57a449dbf71ac957fcb66538933a79fdd739a25c
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2023-10-18 23:04:45 -0300

    criadas funções auxiliares privadas e colocados parâmetros nas funções

--- include/apartamento.h
+++ include/apartamento.h
@@ -3,7 +3,7 @@
 
 #include <list>
 #include <string>
-
+#include <iostream>
 using std::list;
 using std::string;
 
@@ -14,54 +14,60 @@ using std::string;
 
    Consideracoes antes de alterar o codigo:
    - Nenhuma por enquanto
+    
+    Mudanças no último commit:
+    adicionadas funções privadas, adicionados parâmetros nas funções
+    incluindo alguns para dar mais segurança (como data de nascimento
+    pra editar a pessoa)
  */
 
-class Apartamento {
-    public:
-
-    Apartamento(int max_Adm = 1, int max_Morador = 5, int max_Visitante = 10, 
+class Apartamento
+{
+public:
+    Apartamento(int max_Adm = 1, int max_Morador = 5, int max_Visitante = 10,
                 int max_Pet = 3, int max_Veiculo = 2);
-    
-    /* Construtor que permite definir limites máximos para diferentes tipos de 
+
+    /* Construtor que permite definir limites máximos para diferentes tipos de
     pessoas, pets e veículos*/
 
-    void inserir_pessoa ();
+    void inserir_pessoa(string nome, string data_nascimento, string tipo_pessoa);
 
     // Insere uma pessoa
 
-    void inserir_pet ();
+    void inserir_pet(string nome, string raca, string tipo);
 
     // Insere um pet
 
-    void inserir_veiculo ();
+    void inserir_veiculo(string modelo, string placa, string tipo_veiculo);
 
     // Insere um veiculo
 
-    void editar_pessoa (string nome_pessoa);
+    void editar_pessoa(string nome_antigo, string nome_novo, string data_nascimento_antiga,
+                       string data_nascimento_nova, string tipo_pessoa);
 
     // Edita uma pessoa
 
-    void editar_pet (string nome_pet);
+    void editar_pet(string nome_pet, string nome_pet_novo, string raca_antiga, string raca_nova, string tipo);
 
     // Edita um pet
 
-    void editar_veiculo (string placa);
+    void editar_veiculo(string placa_antiga, string placa_nova, string modelo, string tipo_veiculo);
 
     // Edita um veiculo
 
-    void excluir_pessoa (string nome_pessoa);
+    void excluir_pessoa(string nome_pessoa, string data_nascimento);
 
     // Exclui uma pessoa
 
-    void excluir_pet (string nome_pet);
+    void excluir_pet(string nome_pet, string raca);
 
     // Exclui um pet
 
-    void excluir_veiculo (string placa);
+    void excluir_veiculo(string placa);
 
     // Exclui um veiculo
 
-    void exibir_estatisticas ();
+    void exibir_estatisticas();
 
     /*
     Exibe, de um modo geral as informacoes de um apartamento, como:
@@ -70,42 +76,78 @@ class Apartamento {
     - Informações gerais de cada objeto
     */
 
-    private:
-
-    enum TipoPessoa {
+private:
+    enum TipoPessoa
+    {
         administradora,
         moradora,
         visitante,
     };
 
-    struct Pessoa{
+    struct Pessoa
+    {
         string nome;
         string data_nascimento;
         TipoPessoa tipo_pessoa;
     };
 
-    enum TipoPet {
+    enum TipoPet
+    {
         cachorro,
         gato,
         passaro,
         outro,
     };
 
-    struct Pet {
+    struct Pet
+    {
         string nome;
         string raca;
         TipoPet tipo_pet;
     };
 
-    struct Veiculo {
+    struct Veiculo
+    {
         string modelo;
         string placa;
         string tipo_veiculo;
     };
 
-    list <Pessoa> pessoas_;
-    list <Pet> pets_;
-    list <Veiculo> veiculos_;
+    list<Pessoa> pessoas_;
+    list<Pet> pets_;
+    list<Veiculo> veiculos_;
+    int contar_moradores()
+    {
+        int num_moradores = 0;
+        for (auto it = pessoas_.begin(); it != pessoas_.end(); it++)
+        {
+
+            if ((*it).tipo_pessoa == 'moradora')
+            {
+                num_moradores++;
+            }
+        }
+        return num_moradores;
+    }
+
+    int contar_visitantes()
+    {
+        int num_visitantes = 0;
+        for (auto it = pessoas_.begin(); it != pessoas_.end(); it++)
+        {
+            if ((*it).tipo_pessoa == 'visitante')
+            {
+                num_visitantes++;
+            }
+        }
+        return num_visitantes;
+    }
+    /*
+    fiz algumas pesquisas e não tem como declarar essas funções privadas do 
+    jeito que eu queria, que era o mesmo que declara os métodos publicos, se
+    alguém tiver alguma outra ideia será muito bem vinda, mas lembre de que
+    uma função só pode executar uma coisa
+    */
 
     // As variaveis abaixo sao limitadores de pessoas, pets e veiculos
 
