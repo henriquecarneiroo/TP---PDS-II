2aa7b3d22ebbcbd96ff99921d7d4638d3dbb780e
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2023-10-22 01:16:15 -0300

    criado o arquivo que irá armazenar as funções auxiliares

---
+++ src/funcoes_auxiliares.cpp
@@ -0,0 +1,29 @@
+#include "funcoes_auxiliares.h"
+#include "apartamento.h"
+
+int contar_morador(list<Pessoa>::iterator itB, list<Pessoa>::iterator itE)
+{
+    int num_moradores = 0;
+    for (itB;itB != itE; itB++)
+    {
+
+        if ((*itB).tipo_pessoa == "moradora")
+        {
+            num_moradores++;
+        }
+    }
+    return num_moradores;
+}
+
+int contar_visitante(int contar_morador(list<Pessoa>::iterator itB, list<Pessoa>::iterator itE))
+{
+    int num_visitantes = 0;
+    for (itB;itB != itE; itB++)
+    {
+        if ((*itB).tipo_pessoa == "visitante")
+        {
+            num_visitantes++;
+        }
+    }
+    return num_visitantes;
+}
