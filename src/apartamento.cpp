f90363662f432559ab5684c24fb4f3ff7fab7093
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2023-10-22 01:14:24 -0300

    implementado sistema de contagem dos numeros máximos

--- src/apartamento.cpp
+++ src/apartamento.cpp
@@ -15,9 +15,9 @@ vincular o pet e o veículo a um dono
 colocar mais estatisticas
 */
 
-//tem que testar esse construtor pra ver se ele funciona como o esperado
-Apartamento::Apartamento(int max_Adm = 1, int max_Morador = 5, int max_Visitante = 10,
-                         int max_Pet = 3, int max_Veiculo = 2)
+// tem que testar esse construtor pra ver se ele funciona como o esperado
+Apartamento::Apartamento(int max_Adm, int max_Morador, int max_Visitante,
+                         int max_Pet, int max_Veiculo)
 {
     numeroAdm = max_Adm;
     numeroMoradores = max_Morador;
@@ -25,9 +25,10 @@ Apartamento::Apartamento(int max_Adm = 1, int max_Morador = 5, int max_Visitante
     numeroPets = max_Pet;
     numeroVeiculos = max_Veiculo;
 }
-//talvez seria interessante criar uma função que adiciona o tipo_pessoa pra usar aq e no editar pessoa
+// talvez seria interessante criar uma função que adiciona o tipo_pessoa pra usar aq e no editar pessoa
 void Apartamento::inserir_pessoa(string nome, string data_nascimento, string tipo_pessoa)
 {
+
     Pessoa pessoa;
 
     pessoa.nome = nome;
@@ -35,50 +36,74 @@ void Apartamento::inserir_pessoa(string nome, string data_nascimento, string tip
 
     if (tipo_pessoa == "administradora")
     {
-        pessoa.tipo_pessoa = administradora;
+        if (numeroAdm > 0)
+        {
+            pessoa.tipo_pessoa = administradora;
+            numeroAdm--;
+        }
     }
     else if (tipo_pessoa == "moradora")
     {
-        pessoa.tipo_pessoa = moradora;
+        if (numeroMoradores > 0)
+        {
+            pessoa.tipo_pessoa = moradora;
+            numeroMoradores--;
+        }
     }
     else if (tipo_pessoa == "visitante")
     {
-        pessoa.tipo_pessoa = visitante;
+        if (numeroVisitantes > 0)
+        {
+            pessoa.tipo_pessoa = visitante;
+            numeroVisitantes--;
+        }
     }
+    pessoas_.push_back(pessoa);
 }
-//o mesmo vale pra essa...
+// o mesmo vale pra essa...
 void Apartamento::inserir_pet(string nome, string raca, string tipo)
 {
     Pet pet;
-    pet.nome = nome;
-    pet.raca = raca;
+    if (numeroPets > 0)
+    {
 
-    if (tipo == "cachorro")
-    {
-        pet.tipo_pet = cachorro;
+        pet.nome = nome;
+        pet.raca = raca;
+
+        if (tipo == "cachorro")
+        {
+            pet.tipo_pet = cachorro;
+        }
+        else if (tipo == "gato")
+        {
+            pet.tipo_pet = cachorro;
+        }
+        else if (tipo == "passaro")
+        {
+            pet.tipo_pet = passaro;
+        }
+        else if (tipo == "outro")
+        {
+            pet.tipo_pet = outro;
+        }
+        pets_.push_back(pet);
+
+        numeroPets--;
     }
-    else if (tipo == "gato")
-    {
-        pet.tipo_pet = cachorro;
-    }
-    else if (tipo == "passaro")
-    {
-        pet.tipo_pet = passaro;
-    }
-    else if (tipo == "outro")
-    {
-        pet.tipo_pet = outro;
-    }
-    pets_.push_back(pet);
 }
 
 void Apartamento::inserir_veiculo(string modelo, string placa, string tipo_veiculo)
 {
-    Veiculo veiculo;
-    veiculo.modelo = modelo;
-    veiculo.placa = placa;
-    veiculo.tipo_veiculo = tipo_veiculo;
-    veiculos_.push_back(veiculo);
+    if (numeroVeiculos > 0)
+    {
+        Veiculo veiculo;
+        veiculo.modelo = modelo;
+        veiculo.placa = placa;
+        veiculo.tipo_veiculo = tipo_veiculo;
+        veiculos_.push_back(veiculo);
+
+        numeroVeiculos--;
+    }
 }
 void Apartamento::editar_pessoa(string nome_antigo, string nome_novo, string data_nascimento_antiga,
                                 string data_nascimento_nova, string tipo_pessoa)
@@ -160,6 +185,7 @@ void Apartamento::excluir_pessoa(string nome_pessoa, string data_nascimento)
         {
             pessoas_.erase(it);
             break;
+            // colocar alguma forma de verificar se o tipo de pessoa pra mudar o numro restante
         }
     }
 }
@@ -172,6 +198,7 @@ void Apartamento::excluir_pet(string nome_pet, string raca)
         {
             pets_.erase(it);
             break;
+            numeroPets ++;
         }
     }
 }
@@ -183,17 +210,18 @@ void Apartamento::excluir_veiculo(string placa)
         if ((*it).placa == placa)
         {
             veiculos_.erase(it);
+            numeroVeiculos ++;
         }
     }
 }
 
 void Apartamento::exibir_estatisticas()
 {
-    cout << "Quantidades de moradores do prédio: " << contar_moradores();
-    cout << "Quantidade de visitantes: " << contar_visitantes();
-    cout << "Quantidade de pets: " << pets_.size();
-    cout << "Quantidade de veículos: " << veiculos_.size();
-
+    cout << "Quantidades de moradores do prédio: "; //<< contar_moradores() << endl;
+    cout << "Quantidade de visitantes: ";           //<< contar_visitantes() << endl;
+    cout << "Quantidade de pets: " << pets_.size() << endl;
+    cout << "Quantidade de veículos: " << veiculos_.size() << endl;
+    /*implementar as funções acima*/
     /*
     Se tiver mais alguma coisa que vocês quiserem adicionar nessa função fiquem a vontade,
     lembrem de sempre usar o encapsulamento.
