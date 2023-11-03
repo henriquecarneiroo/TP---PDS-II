4dd5fdcd39b85e0f4f1173fa20949a993d0936d6
Igor Vinicius <142598070+IgorViniciusPinto@users.noreply.github.com>
2023-11-03 08:32:10 -0300

    Mudanças no encomendas.cpp
    Tentei fazer algumas mudanças no encomendas.cpp para fazer com que funcione de acordo com o novo main.cpp e apartamentos.cpp, mas como fiz meio corrido não tenho confiança de que funcione melhor do que o código que havia antes, então vou deixar comentado. Assim que tiver mais tempo volto e tento corrigir qualquer problema que ele gere e se for possível colocar como definitivo.

--- src/encomendas.cpp
+++ src/encomendas.cpp
@@ -67,3 +67,64 @@ bool Encomendas::verificar_encomenda(string id)
     }
     return false;
 }
+
+
+/*
+#include "encomendas.hpp"
+#include "apartamento.hpp"
+#include <iostream>
+
+// verifica se o tamanho do ID da encomenda está fora do intervalo desejado
+bool verifica_id(string id) {
+    return id.size() >= 6 && id.size() <= 50;
+}
+
+bool Encomendas::registrar_encomenda(string id, string destinatario, string data_entrega) {
+    Apartamento ap;
+
+    if (!verifica_id(id)) {
+        std::cerr << "Erro ao registrar encomenda: ID de encomenda inválido." << std::endl;
+        return false;
+    }
+
+    if (!ap.eh_morador(destinatario)) {
+        std::cerr << "Erro ao registrar encomenda: Destinatário não é um morador válido." << std::endl;
+        return false;
+    }
+
+    // Declaração da variável encomendas_ na classe Encomendas
+    Encomenda encomenda;
+    encomenda.data_entrega = data_entrega;
+    encomenda.destinatario = destinatario;
+
+    encomendas_[id] = encomenda;
+    return true;
+}
+
+void Encomendas::apagar_encomenda(string id) {
+    auto it = encomendas_.find(id);
+    if (it == encomendas_.end()) {
+        std::cerr << "Erro ao apagar encomenda: ID de encomenda não encontrado." << std::endl;
+        return;
+    }
+
+    encomendas_.erase(it);
+}
+
+void Encomendas::exibir_encomendas() {
+    for (const auto& entry : encomendas_) {
+        cout << "ID: " << entry.first << endl;
+        cout << "Destinatário: " << entry.second.destinatario << endl;
+        cout << "Data de Entrega: " << entry.second.data_entrega << endl;
+        cout << "-------------------------" << endl;
+    }
+}
+
+bool Encomendas::verificar_encomenda(string id) {
+    if (encomendas_.find(id) == encomendas_.end()) {
+        std::cerr << "Erro ao verificar encomenda: ID de encomenda não encontrado." << std::endl;
+        return false;
+    }
+    return true;
+}
+*/
